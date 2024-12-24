eb18432e594fe09ec7384293c41a60886bce691e
KYBEX <artm.22@internet.ru>
2024-12-24 19:12:40 +0300

    updaateee

--- engine/src-tauri/src/lib.rs
+++ engine/src-tauri/src/lib.rs
@@ -9,6 +9,20 @@ fn create_project(_project_name: String, _project_type: String, _template: Strin
 }
 
 #[tauri::command]
+fn return_files_and_directories(_path: String) -> Result<Vec<String>, String> {
+    let entries = std::fs::read_dir(_path).map_err(|e| e.to_string())?;
+    let mut files_and_dirs = Vec::new();
+
+    for entry in entries {
+        let entry = entry.map_err(|e| e.to_string())?;
+        let name = entry.file_name().to_string_lossy().into_owned();
+        files_and_dirs.push(name);
+    }
+    
+    Ok(files_and_dirs)
+}
+
+#[tauri::command]
 fn create_recently() -> Result<(), String> {
     let user_name = env::var("USERNAME").map_err(|e| e.to_string())?;
     let path = format!("C:\\Users\\{}\\AppData\\Roaming\\WhereEngine\\recently.json", user_name);
@@ -29,7 +43,7 @@ fn get_recently() -> Result<String, String> {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![create_project, get_recently, create_recently])
+        .invoke_handler(tauri::generate_handler![create_project, get_recently, create_recently, return_files_and_directories])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- engine/src/pages/project/Open.tsx
+++ engine/src/pages/project/Open.tsx
@@ -1,23 +1,37 @@
 import { useState } from "react";
-import Input from "../../UIKit/Input";
 import Button from "../../UIKit/Button";
+import { OpenProject } from "../../tools/project";
+import { invoke } from "@tauri-apps/api/core";
+import Input from "../../UIKit/Input";
 
 export default function Open() {
     const [path, setPath] = useState('');
+    const [filesAndDirs, setFilesAndDirs] = useState<string[]>([]);
+
+    const handlePathChange = (newPath: string) => {
+        setPath(newPath);
+        invoke('return_files_and_directories', { path: newPath })
+            .then((message) => setFilesAndDirs(message as string[]))
+            .catch((error) => console.error(error));
+    };
+
     return (
         <>
             <div className="justify-center items-center flex h-screen">
                 <div className="w-auto">
                     <h1 className="text-white text-4xl p-10 text-center font-bold">Open Project</h1>
                     <div className="bg-white w-96 h-96 rounded-xl shadow-lg p-4">
-                        <div className="flex items-center gap-2 justify-center">
-                            <Input placeholder="Project Path" onchange={(e) => setPath(e.target.value)} />
-                            <Button text="Open" func={() => console.log('pipiska')}/>
+                        <div className="items-center gap-2 justify-center p-4">
+                            <Input placeholder="Project Path" onchange={(e) => handlePathChange(e.target.value)} />
                         </div>
-                        <div className="bg-black rounded-xl w-90 h-60">
-                            <h1 className="text-white p-5 font-bold text-lg">penis</h1>
+                        <div className="bg-black rounded-xl overflow-x-hidden overflow-y-auto w-full h-60">
+                            {filesAndDirs.map((file, index) => (
+                                <h1 key={index} className="text-white p-1">{file}</h1>
+                            ))}
                         </div>
                     </div>
+                    <Button text="Open" func={() => OpenProject(path)}/>
+                    <Button text="Back" func={() => window.location.href = "/"}/>
                 </div>
             </div>
         </>
